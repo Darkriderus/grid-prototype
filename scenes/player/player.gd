69cdb316a345d571ad8315696ccbc86461fe46ce
Philipp Sorio <6162383+Darkriderus@users.noreply.github.com>
2026-02-18 13:21:51 +0100

    .

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-const TILE_SIZE: Vector2 = Vector2(32, 32)
 var tween: Tween
 
 @onready var up: RayCast2D = $Up
@@ -9,7 +8,7 @@ var tween: Tween
 @onready var right: RayCast2D = $Right
 @onready var sprite: Sprite2D = $Sprite
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if !tween or !tween.is_running():
 		if Input.is_action_pressed("ui_up") and !up.is_colliding():
 			_move(Vector2.UP)
@@ -21,8 +20,8 @@ func _physics_process(delta: float) -> void:
 			_move(Vector2.RIGHT)
 
 func _move(direction: Vector2) -> void:
-	global_position += direction * TILE_SIZE
-	sprite.global_position -= direction * TILE_SIZE
+	global_position += direction * Constants.TILE_SIZE
+	sprite.global_position -= direction * Constants.TILE_SIZE
 	
 	if tween:
 		tween.kill()
---
+++ scripts/constants.gd
@@ -0,0 +1,4 @@
+class_name Constants
+extends Node
+
+const TILE_SIZE := Vector2(32, 32)
