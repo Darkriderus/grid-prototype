c92e2e6e30475436911d93a61334763105080c95
Philipp Sorio <philipp.sorio@hotmail.com>
2026-02-18 18:45:17 +0100

    adds diagonal movement

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,22 +2,43 @@ extends CharacterBody2D
 
 var tween: Tween
 
-@onready var up: RayCast2D = $Up
-@onready var down: RayCast2D = $Down
-@onready var left: RayCast2D = $Left
-@onready var right: RayCast2D = $Right
 @onready var sprite: Sprite2D = $Sprite
 
+@onready var up: RayCast2D = $Up
+@onready var up_right: RayCast2D = $UpRight
+@onready var right: RayCast2D = $Right
+@onready var down_right: RayCast2D = $DownRight
+@onready var down: RayCast2D = $Down
+@onready var down_left: RayCast2D = $DownLeft
+@onready var left: RayCast2D = $Left
+@onready var up_left: RayCast2D = $UpLeft
+
+
 func _physics_process(_delta: float) -> void:
-	if !tween or !tween.is_running():
-		if Input.is_action_pressed("ui_up") and !up.is_colliding():
-			_move(Vector2.UP)
-		if Input.is_action_pressed("ui_down") and !down.is_colliding():
-			_move(Vector2.DOWN)
-		if Input.is_action_pressed("ui_left") and !left.is_colliding():
-			_move(Vector2.LEFT)
-		if Input.is_action_pressed("ui_right") and !right.is_colliding():
-			_move(Vector2.RIGHT)
+	if not tween or not tween.is_running():
+		if Input.is_action_pressed("move_up") and not up.is_colliding():
+			_move(Direction.UP)
+		
+		if Input.is_action_pressed("move_up_right") and (not up_right.is_colliding()):
+			_move(Direction.UP_RIGHT)
+			
+		if Input.is_action_pressed("move_right") and not right.is_colliding():
+			_move(Direction.RIGHT)
+			
+		if Input.is_action_pressed("move_down_right") and (not down_right.is_colliding()):
+			_move(Direction.DOWN_RIGHT)
+			
+		if Input.is_action_pressed("move_down") and not down.is_colliding():
+			_move(Direction.DOWN)
+			
+		if Input.is_action_pressed("move_down_left") and (not down_left.is_colliding()):
+			_move(Direction.DOWN_LEFT)
+			
+		if Input.is_action_pressed("move_left") and not left.is_colliding():
+			_move(Direction.LEFT)
+
+		if Input.is_action_pressed("move_up_left") and (not up_left.is_colliding()):
+			_move(Direction.UP_LEFT)
 
 func _move(direction: Vector2) -> void:
 	global_position += direction * Constants.TILE_SIZE
---
+++ scripts/direction.gd
@@ -0,0 +1,11 @@
+class_name Direction
+extends Node
+
+const UP := Vector2.UP
+const UP_RIGHT := Vector2.UP + Vector2.RIGHT
+const RIGHT := Vector2.RIGHT
+const DOWN_RIGHT := Vector2.DOWN + Vector2.RIGHT
+const DOWN := Vector2.DOWN
+const DOWN_LEFT := Vector2.DOWN + Vector2.LEFT
+const LEFT := Vector2.LEFT
+const UP_LEFT := Vector2.UP + Vector2.LEFT
