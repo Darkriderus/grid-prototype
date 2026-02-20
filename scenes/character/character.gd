04b1318d6734c326e676818ade1cab7e3991be8a
Philipp Sorio <philipp.sorio@hotmail.com>
2026-02-20 15:13:17 +0100

    character cleanup

--- scenes/character/character.gd
+++ scenes/character/character.gd
@@ -1,9 +1,8 @@
 class_name Character
 extends CharacterBody2D
 
-@onready var sprite: Sprite2D = $Sprite
 @export var sight_radius: Area2D
-@onready var animation_player: AnimationPlayer = %AnimationPlayer
+@onready var char_sprite: CharSprite = $CharSprite
 
 var tween: Tween
 
@@ -13,23 +12,19 @@ func _ready() -> void:
 	
 	TurnManager.add_character(self)
 	
-	animation_player.stop()
-	animation_player.play("idle")
+	char_sprite.play_animation("characterbody/idle")
 
 func move(direction: Vector2) -> void:
-	animation_player.stop()
-	animation_player.play("schmirb_walk")
-	
+	char_sprite.play_animation("characterbody/schmirb_walk")
 	global_position += direction * Constants.TILE_SIZE
-	sprite.global_position -= direction * Constants.TILE_SIZE
+	char_sprite.sprite.global_position -= direction * Constants.TILE_SIZE
 	
 	tween = create_tween()
 	tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-	tween.tween_property(sprite, "global_position", global_position, Constants.MOVE_SPEED).set_trans(Tween.TRANS_SINE)
+	tween.tween_property(char_sprite.sprite, "global_position", global_position, Constants.MOVE_SPEED).set_trans(Tween.TRANS_SINE)
 	tween.tween_callback(_turn_end)
 	
 func _turn_end():
-	animation_player.stop()
-	animation_player.play("idle")
+	char_sprite.play_animation("characterbody/idle")
 	
 	print("Char Turn ended: ", name)
