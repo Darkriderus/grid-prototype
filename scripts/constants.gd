a838ad34ee7c26bfe7c72292139f4b71ccc0df82
Philipp Sorio <6162383+Darkriderus@users.noreply.github.com>
2026-03-03 11:42:19 +0100

    reset

--- scripts/constants.gd
+++ scripts/constants.gd
@@ -1,6 +1,5 @@
 class_name Constants
 extends Node
 
-const TILE_WITDH := 32
+const TILE_WITDH := 16
 const TILE_SIZE := Vector2(TILE_WITDH, TILE_WITDH)
-const MOVE_SPEED := 0.25
--- scripts/signals.gd
+++ scripts/signals.gd
@@ -1,6 +1,1 @@
 extends Node
-
-signal character_added(character: CharacterBody2D)
-signal character_removed(character: CharacterBody2D)
-signal turn_started(character: CharacterBody2D)
-signal turn_ended(character: CharacterBody2D)
