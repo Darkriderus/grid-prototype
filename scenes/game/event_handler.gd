d23e1a940dcc32f1c5d302133219a562694b8395
Philipp Sorio <6162383+Darkriderus@users.noreply.github.com>
2026-03-03 16:08:43 +0100

    base

---
+++ scenes/game/event_handler.gd
@@ -0,0 +1,20 @@
+class_name EventHandler
+extends Node
+
+
+func get_action() -> Action:
+	var action: Action = null
+	
+	if Input.is_action_just_pressed("move_up"):
+		action = MovementAction.new(0, -1)
+	elif Input.is_action_just_pressed("move_down"):
+		action = MovementAction.new(0, 1)
+	elif Input.is_action_just_pressed("move_left"):
+		action = MovementAction.new(-1, 0)
+	elif Input.is_action_just_pressed("move_right"):
+		action = MovementAction.new(1, 0)
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		action = EscapeAction.new()
+	
+	return action
--- scripts/constants.gd
+++
@@ -1,5 +0,0 @@
-class_name Constants
-extends Node
-
-const TILE_WITDH := 16
-const TILE_SIZE := Vector2(TILE_WITDH, TILE_WITDH)
