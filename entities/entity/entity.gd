4044015c858604aed66d385e84c00133f230f46a
Philipp Sorio <6162383+Darkriderus@users.noreply.github.com>
2026-03-04 16:41:30 +0100

    adds pickup of items

--- entities/entity/entity.gd
+++ entities/entity/entity.gd
@@ -14,9 +14,11 @@ var _definition: EntityDefinition
 var entity_name: String
 var blocks_movement: bool
 var map_data: MapData
+
 var fighter_component: FighterComponent
 var ai_component: BaseAIComponent
 var consumable_component: ConsumableComponent
+var inventory_component: InventoryComponent
 
 func _init(map_data: MapData, start_position: Vector2i, entity_definition: EntityDefinition) -> void:
 	centered = false
@@ -45,6 +47,10 @@ func set_entity_type(entity_definition: EntityDefinition) -> void:
 		if entity_definition.consumable_definition is HealingConsumableComponentDefinition:
 			consumable_component = HealingConsumableComponent.new(entity_definition.consumable_definition)
 			add_child(consumable_component)
+	
+	if entity_definition.inventory_capacity > 0:
+		inventory_component = InventoryComponent.new(entity_definition.inventory_capacity)
+		add_child(inventory_component)
 	
 	
 var grid_position: Vector2i:
--- entities/map_data/map_data.gd
+++ entities/map_data/map_data.gd
@@ -1,6 +1,8 @@
 class_name MapData
 extends RefCounted
 
+signal entity_placed(entity)
+
 const tile_types = {
 	"floor": preload("uid://bby8nrjwno8kf"),
 	"wall": preload("uid://cxxd6wrpw0pu7"),
@@ -97,3 +99,10 @@ func get_actor_at_location(location: Vector2i) -> Entity:
 		if actor.grid_position == location:
 			return actor
 	return null
+
+func get_items() -> Array[Entity]:
+	var items: Array[Entity] = []
+	for entity in entities:
+		if entity.consumable_component != null:
+			items.append(entity)
+	return items
