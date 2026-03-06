11fc6e790cb015dfcb50dbac489370d54ff466d8
Philipp Sorio <philipp.sorio@hotmail.com>
2026-03-06 17:47:41 +0100

    adds lvlups

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -4,6 +4,7 @@ extends Node2D
 signal player_created(player)
 
 const player_definition: EntityDefinition = preload("uid://dgtkb8ig8pwt0")
+const level_up_menu_scene: PackedScene = preload("uid://snopwjqg3h3j")
 
 @onready var player: Entity
 @onready var input_handler: InputHandler = $InputHandler
@@ -12,6 +13,7 @@ const player_definition: EntityDefinition = preload("uid://dgtkb8ig8pwt0")
 
 func new_game() -> void:
 	player = Entity.new(null, Vector2i.ZERO, "player")
+	player.level_component.level_up_required.connect(_on_player_level_up_requested)
 	player_created.emit(player)
 	remove_child(camera)
 	player.add_child(camera)
@@ -29,6 +31,7 @@ func load_game() -> bool:
 	player.add_child(camera)
 	if not map.load_game(player):
 		return false
+	player.level_component.level_up_required.connect(_on_player_level_up_requested)
 	player_created.emit(player)
 	map.update_fov(player.grid_position)
 	MessageLog.send_message.bind(
@@ -53,3 +56,12 @@ func _handle_enemy_turns() -> void:
 
 func get_map_data() -> MapData:
 	return map.map_data
+	
+	
+func _on_player_level_up_requested() -> void:
+	var level_up_menu: LevelUpMenu = level_up_menu_scene.instantiate()
+	add_child(level_up_menu)
+	level_up_menu.setup(player)
+	set_physics_process(false)
+	await level_up_menu.level_up_completed
+	set_physics_process.bind(true).call_deferred()
