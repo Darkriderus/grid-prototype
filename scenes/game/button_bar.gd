c4d2ae00dcfe3bb2d55f3ba8a54202ee248babc3
Philipp Sorio <6162383+Darkriderus@users.noreply.github.com>
2026-03-12 12:22:49 +0100

    basic ui bar

---
+++ scenes/game/button_bar.gd
@@ -0,0 +1,12 @@
+class_name ButtonBar
+extends PanelContainer
+
+@onready var inventory_button: Button = %InventoryButton
+@onready var character_button: Button = %CharacterButton
+
+
+func _on_character_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_inventory_button_pressed() -> void:
+	pass # Replace with function body.
