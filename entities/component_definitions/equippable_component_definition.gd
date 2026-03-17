b2dd27927c9af42c5809218b35d560e518f67987
Philipp Sorio <6162383+Darkriderus@users.noreply.github.com>
2026-03-17 16:39:28 +0100

    adds equipmentstats for the new combat system

--- entities/component_definitions/equippable_component_definition.gd
+++ entities/component_definitions/equippable_component_definition.gd
@@ -1,8 +1,15 @@
 class_name EquippableComponentDefinition
 extends ItemComponentDefinition
 
+@export_category("Common")
 @export var equipment_type: EquippableComponent.EquipmentType
-@export var melee_power_bonus: int = 0
-@export var ranged_power_bonus: int = 0
+
+@export_category("Armor")
+@export var protection: int = 0
+@export var dodge_chance: float = 0.0
+
+@export_category("Weapons")
+@export var two_handed: bool = false
+@export var min_damage: int = 0
+@export var max_damage: int = 0
 @export var attack_range: int = 1
-@export var defense_bonus: int = 0
